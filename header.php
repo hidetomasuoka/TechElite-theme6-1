bd2fd7bfa54a17499c9f74c4cffe6007a1089b67
Hideto Masuoka <hidemasuoka112@gmail.com>
2025-04-14 00:09:57 +0900

    修正

--- header.php
+++ header.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <!-- css-->
-    <link rel="stylesheet" href="css/main_new.css">
+    <link rel="stylesheet" href="css/main.css">
     <link rel="stylesheet" href="css/responsive.css">
     <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@100;300;400;500;700;900&display=swap" rel="stylesheet">
     <title>フリーランスサミット</title>
--- index.php
+++ index.php
@@ -11,7 +11,8 @@
                     <img src="../img/kv.svg" alt="フリーランスサミット">
                 </h1>
                 <div class="kv__date">
-                    3/4 <small class="kv__date__small">Sat</small> 10:00-18:00
+                    3/4 <small class="kv__date__small">Sat</small> 
+                    <div class="kv__time">10:00-18:00</div>
                 </div>
                 <div class="kv__location">
                     東京国際フォーラム  ホールE2
